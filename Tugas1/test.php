6c62c239e2cf9cd755684e9f8794148d5616389a
NaoviMagfiroh <80589413+NaoviMagfiroh@users.noreply.github.com>
2022-03-01 21:42:50 +0700

    Add files via upload

---
+++ Tugas1/test.php
@@ -0,0 +1,23 @@
+<?php
+
+  #for($i=1; $i<=10; $i++) {
+  #  print("Urutan ke-$i\n");
+  #}
+  
+  $nama = "Naovi Magfiroh";
+  $nim = "20090135";
+  $kelas = "4D";
+  echo "Perkenalkan <br/>
+  Nama Saya : $nama <br/>
+  NIM saya : $nim <br/>
+  Kelas : $kelas <br/>";
+
+  #$output = shell_exec("ls -lart");
+  #echo $output;
+
+  #shell_exec("touch index.php");
+  #shell_exec("echo '<h1>Hai, Apa kabar</h1>' > index.php");
+  print("Salam Kenal dari Saya $nama\n");
+  
+  
+?>
